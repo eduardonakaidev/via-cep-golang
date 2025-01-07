2d398a4de9b172b4c994f93339ab1ddafa751f50
My Little Pet <mylittlepetinternal@gmail.com>
2025-01-07 16:28:39 -0300

    git commit -m "feat: add HTTP handlers for health check and CEP endpoint"

---
+++ internal/handlers/cep-handler.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"encoding/json"
+
+	"github.com/eduardofrnkdev/via-cep-golang/internal/services"
+	"github.com/gorilla/mux"
+
+	"net/http"
+)
+
+// SetupRoutes initializes the router and defines all routes.
+func SetupRoutes() *mux.Router {
+	router := mux.NewRouter()
+
+	// Health check route
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ping"))
+	})
+
+	// CEP route
+	router.HandleFunc("/cep/{id}", GetCepHandler)
+
+	return router
+}
+
+// GetCepHandler handles requests to fetch CEP information.
+func GetCepHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+	cep, err := services.GetCep(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error(), "Error fetching CEP")
+		return
+	}
+	w.Write([]byte(cep))
+}
+
+// respondWithError sends an error response with a JSON payload.
+func respondWithError(w http.ResponseWriter, code int, errorMsg, message string) {
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{
+		"error":   errorMsg,
+		"message": message,
+	})
+}
